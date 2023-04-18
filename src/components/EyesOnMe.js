18bf9b6dcf61d317f7b8bc4a8da783e963034a86
Tina Shi <init.defaultBranch>
2023-04-18 20:50:14 +1000

    adjustments to be made to pass tests...

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+function handleFocus() {
+    console.log('Good!')
+}
+
+function handleBlur() {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe() {
+    return (
+        <button 
+            onFocus= {handleFocus}
+            onBlur= {handleBlur}
+            >Eyes On Me</button>
+    )
+}
+
+export default EyesOnMe;
+
